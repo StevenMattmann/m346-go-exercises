348770d275be98035e99325e730ef195fb69168c
Steven Mattmann <steven.mattmann07@gmail.com>
2024-11-18 09:28:41 +0100

    Solved first two exercises

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	var firstName string = "Max"
+	var lastName string = "Mustermann"
+	var dayOfBirth int = 30
+	var monthOfBirth int = 10
+	var yearOfBirth int = 1999
+	var numberOfSiblings int = 3
+	var heightInMeters float64 = 1.82
+	var zodiacSign rune = '\u264F'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
