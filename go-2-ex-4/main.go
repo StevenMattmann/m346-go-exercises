55a8f4dc937dd3bf963f32879fd80b823b51d232
Steven Mattmann <steven.mattmann07@gmail.com>
2024-12-02 09:14:54 +0100

    Solved exercise

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -2,20 +2,18 @@ package main
 
 import "fmt"
 
-// Student repräsentiert einen Schüler mit Vor- und Nachnamen.
 type Student struct {
 	FirstName string
 	LastName  string
 }
 
-// Class repräsentiert eine Klasse, die aus mehreren Schülern besteht.
 type Class struct {
 	Name     string
 	Students []Student
 }
 
 func main() {
-	// Erstellung von zwei Klassen mit jeweils drei Schülern.
+
 	class1 := Class{
 		Name: "INA23bL",
 		Students: []Student{
@@ -34,19 +32,16 @@ func main() {
 		},
 	}
 
-	// Erstellung von Modulen und Zuordnung der Klassen.
 	modules := map[int][]Class{
-		346: {class1, class2}, // Modul wird von beiden Klassen besucht.
-		104: {class1},         // Modul wird nur von Klasse 1A besucht.
-		117: {class2},         // Modul wird nur von Klasse 2B besucht.
+		346: {class1, class2},
+		104: {class1},
+		117: {class2},
 	}
 
-	// Ausgabe der Klassen und ihrer Schüler.
 	fmt.Println("Klassen:")
 	fmt.Printf("Klasse %s: %+v\n", class1.Name, class1.Students)
 	fmt.Printf("Klasse %s: %+v\n", class2.Name, class2.Students)
 
-	// Ausgabe der Module und der zugehörigen Klassen.
 	fmt.Println("\nModule:")
 	for moduleNumber, attendingClasses := range modules {
 		fmt.Printf("Modul %d wird besucht von:\n", moduleNumber)
